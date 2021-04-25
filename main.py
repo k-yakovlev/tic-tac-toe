a5b9e802d65c5f06fb1da49b2e23e80f521f8938
Kirill Yakovlev <k.a.yakovlev@ya.ru>
2021-04-25 12:41:23 +0300

    Add while loop for changing players.

--- main.py
+++ main.py
@@ -17,12 +17,19 @@ logo = """
 
 """
 
-current_player = 'Player "X"'
+players = ['', 'X', 'O']
+i = 1
 
-print(logo)
-print(3*' ' + f'  {nums[1]}  │  {nums[2]}  │  {nums[3]}  ' + 38*' ' + row_1)
-print(3*' ' + '─────┼─────┼─────' + 38*' ' + '─────┼─────┼─────')
-print(3*' ' + f'  {nums[4]}  │  {nums[5]}  │  {nums[6]}  ' + f'{current_player:^38s}' + row_2)
-print(3*' ' + '─────┼─────┼─────' + 38*' ' + '─────┼─────┼─────')
-print(3*' ' + f'  {nums[7]}  │  {nums[8]}  │  {nums[9]}  ' + 38*' ' + row_3)
-player_input = input(f'{"":>28s}Enter the cell number: ')
+while '' in cells.values():
+    current_player = f'Player "{players[i]}"'
+    i = -i
+
+    print('\n' * 100)
+    print(logo)
+    print(3*' ' + f'  {nums[1]}  │  {nums[2]}  │  {nums[3]}  ' + 38*' ' + row_1)
+    print(3*' ' + '─────┼─────┼─────' + 38*' ' + '─────┼─────┼─────')
+    print(3*' ' + f'  {nums[4]}  │  {nums[5]}  │  {nums[6]}  ' + f'{current_player:^38s}' + row_2)
+    print(3*' ' + '─────┼─────┼─────' + 38*' ' + '─────┼─────┼─────')
+    print(3*' ' + f'  {nums[7]}  │  {nums[8]}  │  {nums[9]}  ' + 38*' ' + row_3)
+    print()
+    player_input = input(f'{"":>28s}Enter the cell number: ')
