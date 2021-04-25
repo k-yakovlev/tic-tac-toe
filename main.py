bd382af0d8c5e3fb13ac5e7581c89da09fbadfa0
Kirill Yakovlev <k.a.yakovlev@ya.ru>
2021-04-25 13:25:20 +0300

    Add player mark in cell when it selected.

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-cells = {1: 'X', 2: 'O', 3: '', 4: '', 5: '', 6: '', 7: '', 8: '', 9: ''}
-nums = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+cells = {'1': '', '2': '', '3': '', '4': '', '5': '', '6': '', '7': '', '8': '', '9': ''}
+nums = {'1': '1', '2': '2', '3': '3', '4': '4', '5': '5', '6': '6', '7': '7', '8': '8', '9': '9'}
 
 player_x_cells = []
 player_o_cells = []
 
-row_1 = f'{cells[1]:^5s}│{cells[2]:^5s}│{cells[3]:^5s}'
-row_2 = f'{cells[4]:^5s}│{cells[5]:^5s}│{cells[6]:^5s}'
-row_3 = f'{cells[7]:^5s}│{cells[8]:^5s}│{cells[9]:^5s}'
 
 logo = """
 ,--------.,--.               ,--.                         ,--.                 
@@ -22,7 +19,9 @@ i = 1
 
 while '' in cells.values():
     current_player = f'Player "{players[i]}"'
-    i = -i
+    row_1 = f'{cells["1"]:^5s}│{cells["2"]:^5s}│{cells["3"]:^5s}'
+    row_2 = f'{cells["4"]:^5s}│{cells["5"]:^5s}│{cells["6"]:^5s}'
+    row_3 = f'{cells["7"]:^5s}│{cells["8"]:^5s}│{cells["9"]:^5s}'
 
     print('\n' * 100)
     print(logo)
@@ -33,5 +32,8 @@ while '' in cells.values():
     print(3*' ' + f'  {nums["7"]}  │  {nums["8"]}  │  {nums["9"]}  ' + 38*' ' + row_3)
     print()
     player_input = input(f'{"":>28s}Enter the cell number: ')
-    if player_input in nums.keys():
+    if player_input in nums.values():
+        cells[player_input] = players[i]
         nums[player_input] = ' '
+
+    i = -i
