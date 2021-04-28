5db6ba652992c2b71c275963497c89c099b16941
Kirill Yakovlev <k.a.yakovlev@ya.ru>
2021-04-28 10:50:10 +0300

    Add list of winning_cells.

--- main.py
+++ main.py
@@ -130,6 +130,10 @@ scores = {}
 player_input = False
 nums = {}
 cells = {}
+winning_cells = [
+    (1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7),
+    (2, 5, 8), (3, 6, 9), (1, 5, 9), (3, 5, 7)
+]
 
 if __name__ == '__main__':
     game()
