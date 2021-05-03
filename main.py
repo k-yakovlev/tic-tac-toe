a807fb12934f563f49457c1b62b9366d02e8dcc7
Kirill Yakovlev <k.a.yakovlev@ya.ru>
2021-05-04 00:15:33 +0300

    Add docstring to is_row_of_3_marks().

--- main.py
+++ main.py
@@ -131,6 +131,7 @@ def save_a_move():
 
 
 def is_row_of_3_marks():
+    """Return True if rows of 3 marks are exist on playing field."""
     global winning_rows
     winning_rows = [_ for _ in winning_cells if _.issubset(players_cells[player])]
     if winning_rows:
